6379328229663fd56c8a54c2d5b356a707b3d6a2
bhoener <brodiehoener@icloud.com>
2026-06-16 09:29:58 -0700

    add main

--- src/main.cpp
+++ src/main.cpp
@@ -4,35 +4,70 @@
 #include "priority_queue.h"
 #include "queue.h"
 #include "process.h"
+#include "scheduler.h"
 #include <random>
 using namespace std;
 
+/**
+ * @file main.cpp
+ * holds the simulation testing logic and benchmarking
+ */
 int main()
 {
-    // temporary test for dispatcher, can be removed if needed
-    QueueADT<Process> * ready_queue = new PriorityQueue<Process>();
-    QueueADT<Process> * waiting_queue = new PriorityQueue<Process>();
-    Dispatcher myDispatcher(ready_queue, waiting_queue);
+    // simulation parameters
+    const unsigned int NUM_PROCESSES_TO_ADD = 100;
+    const unsigned int MAX_CONCURRENT_PROCESSES = 15;
 
-    for (int i = 1; i < 8; i++) {
-        Process newProc(i, "process" + to_string(i), i, rand() % 10 + 1);
-        newProc.admit();
-        ready_queue->add(newProc);
-    }
+    // to simulate high distribution of short bursts and small amount of large bursts
+    // we have either short bursts or long bursts
+    const unsigned short int MAX_LONG_BURST_LENGTH = 500;
+    const unsigned short int MAX_SHORT_BURST_LENGTH = 20;
+    const unsigned short int LONG_BURST_CHANCE = 10; // the probability of a long burst is 1/LONG_BURST_CHANCE
 
-    for (int i = 9; i < 20; i++) {
-        Process newProc(i, "process" + to_string(i), i, rand() % 10 + 1);
+    // cap the simulation length
+    const unsigned int MAX_SIMULATION_STEPS = 10000;
+
+    // initialize scheduler and queue of simulation processes
+    Scheduler *scheduler = new Scheduler(); // new RoundRobin(), new SJF(), etc.
+    Queue<Process> *processes_to_add = new Queue<Process>();
+
+    for (int i = 1; i <= NUM_PROCESSES_TO_ADD; i++)
+    {
+        // simulate whether short or long burst and pick burst length accordingly
+        int burst_length = (rand() % LONG_BURST_CHANCE == 0) ? rand() % MAX_LONG_BURST_LENGTH : rand() % MAX_SHORT_BURST_LENGTH;
+        
+        // add process to queue
+        Process newProc(i, "process_" + to_string(i), i, burst_length);
         newProc.block();
-        waiting_queue->add(newProc);
+        processes_to_add->add(newProc);
     }
 
-    for (int i = 0; i < 50; i++) {
-        myDispatcher.step();
-        cout << myDispatcher.getRunningProcess().toString() << endl;
-    }
-    
+    // run simulation, store steps taken (will stop at MAX_SIMULATION_STEPS if not done)
+    int steps_taken = MAX_SIMULATION_STEPS;
+    for (int i = 0; i < MAX_SIMULATION_STEPS; i++)
+    {
+        // if all processes are completed, exit early
+        if (scheduler->get_process_count == 0 && processes_to_add->isEmpty())
+        {
+            steps_taken = i + 1;
+            break;
+        }
 
-    delete ready_queue;
-    delete waiting_queue;
+        // if there is room, squeeze in another process
+        if (scheduler->get_process_count < MAX_CONCURRENT_PROCESSES)
+            scheduler->load_process(processes_to_add->remove());
+
+        // tick the simulation and output
+        scheduler->step();
+        scheduler->log();
+    }
+
+    // log statistics
+    cout << "==================================" << endl;
+    cout << "Processes in simulation: " << NUM_PROCESSES_TO_ADD << endl;
+    cout << "Steps taken: " << steps_taken << endl;
+
+    delete scheduler;
+    delete processes_to_add;
     return 0;
 }
